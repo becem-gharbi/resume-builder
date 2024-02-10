0f71d772f71af2389021eede3f8fbda3984113d2
becem-gharbi <becem.gharbi@live.com>
2024-02-10 21:03:33 +0100

    chore: download icons

--- components/ResumeCard.vue
+++ components/ResumeCard.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink :to="`/resumes/${id}`">
-    <div class="rounded-lg border-2 border-slate-200 dark:border-slate-800 w-40 h-60 hover:bg-slate-100 dark:hover:bg-slate-900 cursor-pointer flex flex-col gap-4 items-center justify-center">
+    <div class="rounded-lg border-2 border-slate-200 dark:border-slate-800 w-40 h-60 hover:bg-slate-50 dark:hover:bg-slate-900 cursor-pointer flex flex-col gap-4 items-center justify-center">
       <NText class="text-base">
         {{ title }}
       </NText>
